45034e2109a9a551b92d47aca5120373f729115f
unknown <ramiropen.work@gmail.com>
2022-04-23 18:31:15 -0700

    after a while i lsot it

---
+++ src/Context/AlbumView.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
+
+function AlbumView() {
+    const { id } = useParams()
+    const [ albumData, setAlbumData ] = useState([])
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key=>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            {renderSongs}
+        </div>
+    )
+}
+
+import { useNavigate } from 'react-router-dom'
+
+function ...View() {
+    const navigate = useNavigate()
+    ...
+    
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            {navButtons()}
+            {allData}
+        </div>
+    )
+}
